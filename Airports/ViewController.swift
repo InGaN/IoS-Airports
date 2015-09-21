c7b51beacddeb745253e2632eabdb02a87b74508
Kevin van der Vleuten <kpjmvleu@pc131361.local>
2015-09-21 14:16:51 +0200

    Initial Commit

---
+++ Airports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airports
+//
+//  Created by Kevin van der Vleuten on 21/09/15.
+//  Copyright © 2015 Kevin van der Vleuten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
